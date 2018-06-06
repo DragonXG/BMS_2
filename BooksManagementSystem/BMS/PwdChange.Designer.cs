cc0eccd20999f678afa4f25102cb553c65fe6d9c
ihateyoullm <2939469001@qq.com>
2018-06-06 12:05:48 +0800

    密码，

--- BooksManagementSystem/BMS/PwdChange.Designer.cs
+++ BooksManagementSystem/BMS/PwdChange.Designer.cs
@@ -41,35 +41,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(84, 60);
+            this.label1.Location = new System.Drawing.Point(112, 75);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.Size = new System.Drawing.Size(67, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "原密码：";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(84, 108);
+            this.label2.Location = new System.Drawing.Point(112, 135);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.Size = new System.Drawing.Size(67, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "新密码：";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(84, 155);
+            this.label3.Location = new System.Drawing.Point(112, 194);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.Size = new System.Drawing.Size(97, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "确认新密码：";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(207, 223);
+            this.button1.Location = new System.Drawing.Point(276, 279);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 29);
             this.button1.TabIndex = 3;
             this.button1.Text = "确定";
             this.button1.UseVisualStyleBackColor = true;
@@ -77,39 +81,48 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(86, 223);
+            this.button2.Location = new System.Drawing.Point(115, 279);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 29);
             this.button2.TabIndex = 4;
             this.button2.Text = "取消";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(167, 57);
+            this.textBox1.Location = new System.Drawing.Point(223, 71);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(126, 21);
+            this.textBox1.Size = new System.Drawing.Size(167, 25);
             this.textBox1.TabIndex = 5;
+            this.textBox1.UseSystemPasswordChar = true;
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(167, 105);
+            this.textBox2.Location = new System.Drawing.Point(223, 131);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(126, 21);
+            this.textBox2.Size = new System.Drawing.Size(167, 25);
             this.textBox2.TabIndex = 6;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(167, 152);
+            this.textBox3.Location = new System.Drawing.Point(223, 190);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(126, 21);
+            this.textBox3.Size = new System.Drawing.Size(167, 25);
             this.textBox3.TabIndex = 7;
+            this.textBox3.UseSystemPasswordChar = true;
+            this.textBox3.MouseLeave += new System.EventHandler(this.textBox3_MouseLeave);
             // 
             // PwdChange
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(365, 293);
+            this.ClientSize = new System.Drawing.Size(487, 366);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -119,6 +132,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PwdChange";
             this.Text = "密码修改";
             this.ResumeLayout(false);
--- BooksManagementSystem/BMS/open_mysql_llm.cs
+++ BooksManagementSystem/BMS/open_mysql_llm.cs
@@ -14,4 +14,10 @@ namespace BMS
         public static MySqlConnection conn = new MySqlConnection(strConn);
 
     }
+    public static class get_number_llm
+    {
+        public static int reader_admin = 0;  //看到底是什么要修改密码，0为读者，1为管理员
+        public static String cardnum = "0";  //获得账号
+        public static String denlumima = "0";  //获得登录密码
+    }
 }
